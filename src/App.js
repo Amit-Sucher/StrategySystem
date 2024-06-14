5722732b0411b86be0c33dff67364b987053f686
AmitSu <sucher.amit@gmail.com>
2024-06-14 19:16:53 +0300

    refresh automatically every 10 seconds

--- src/App.js
+++ src/App.js
@@ -7,23 +7,29 @@ function App() {
     const [teamNumber, setTeamNumber] = useState('');
     const [teamData, setTeamData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRojRhLgZSPXJopPdni1V4Z-inXXY3a__2NaVMsoJHPs9d25ZQ7t56QX67mncr6yo-w4B8WCWyHFe2m/pub?output=csv';
-            Papa.parse(publicSpreadsheetUrl, {
-                download: true,
-                header: true,
-                complete: function(results) {
-                    console.log('Fetched data:', results.data); // Console log to verify data fetching
-                    setData(results.data);
-                },
-                error: function(error) {
-                    console.warn('Error fetching data from Google Sheets', error);
-                }
-            });
-        };
+    const fetchData = async () => {
+        const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRojRhLgZSPXJopPdni1V4Z-inXXY3a__2NaVMsoJHPs9d25ZQ7t56QX67mncr6yo-w4B8WCWyHFe2m/pub?output=csv';
+        Papa.parse(publicSpreadsheetUrl, {
+            download: true,
+            header: true,
+            complete: function(results) {
+                console.log('Fetched data:', results.data); // Console log to verify data fetching
+                setData(results.data);
+            },
+            error: function(error) {
+                console.warn('Error fetching data from Google Sheets', error);
+            }
+        });
+    };
 
+    useEffect(() => {
         fetchData();
+
+        // Set up a periodic fetch every 10 seconds (10000 milliseconds)
+        const intervalId = setInterval(fetchData, 10000);
+
+        // Clean up the interval on component unmount
+        return () => clearInterval(intervalId);
     }, []);
 
     const handleInputChange = (event) => {
@@ -46,7 +52,7 @@ function App() {
             />
             {teamData && (
                 <div className="team-data-container">
-                    <h2>Auto notes: {teamData['Auto Notes']}</h2> {/* Replace 'Auto NotesB' with the actual column name */}
+                    <h2>Auto notes: {teamData['Auto Notes']}</h2> {/* Replace 'Auto Notes' with the actual column name */}
                     <h2>Notes overall: {teamData['Notes']}</h2> {/* Replace 'Notes' with the actual column name */}
                 </div>
             )}
