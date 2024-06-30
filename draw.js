4844478d54c06277f23d064178e05a815f289c09
AmitSu <sucher.amit@gmail.com>
2024-06-30 18:21:13 +0300

    Updates

--- draw.js
+++ draw.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react.js';
 import ReactDOM from 'react-dom';
 import './index.css'; // Ensure the correct path to the CSS file
 import DrawScreen from '../src/DrawScreen'; // Ensure the correct path to DrawScreen component
